3eb7ccb9f322e457190938e7c5562e3e29a6dd7c
NguyenDucTin <99305494+Yonko08042002@users.noreply.github.com>
2022-09-13 17:46:06 +0700

    sc

--- app/src/main/java/com/google/myapp2/MainActivity.java
+++ app/src/main/java/com/google/myapp2/MainActivity.java
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton btn_login;
+    private Button btn_goto_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_login = findViewById(R.id.btn_login);
+        btn_goto_register = findViewById(R.id.btn_goto_register);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent =new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_goto_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,SignupActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/google/myapp2/SignupActivity.java
@@ -0,0 +1,27 @@
+package com.google.myapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private Button btn_back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(SignupActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
