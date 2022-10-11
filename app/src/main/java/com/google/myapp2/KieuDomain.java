118f192a6c76226937200d5acbe8e4ba827b43d0
NguyenDucTin <99305494+Yonko08042002@users.noreply.github.com>
2022-10-11 23:48:58 +0700

    commit lan 3

---
+++ app/src/main/java/com/google/myapp2/KieuDomain.java
@@ -0,0 +1,27 @@
+package com.google.myapp2;
+
+public class KieuDomain {
+    private String  title;
+    private String  url;
+
+    public KieuDomain(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ app/src/main/java/com/google/myapp2/MostViewDomain.java
@@ -0,0 +1,37 @@
+package com.google.myapp2;
+
+public class MostViewDomain {
+    private String title;
+    private String subtitle;
+    private String url;
+
+    public MostViewDomain(String title, String subtitle, String url) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
